1415bf1bced0f89e1e8d878f009cfa9b5a6bc64b
jimmy omar <jimmy@gmail.com>
2022-10-26 19:59:39 -0500

    Agregar archivos de proyecto.

---
+++ JimmyToapanta/Login.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace JimmyToapanta
+{
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            string usuario = "1";
+            string contraseña = "1";
+            string tUsuario = txtUsuario.Text;
+            string tContraseña = txtContraseña.Text;
+            if (usuario == tUsuario & contraseña == tContraseña)
+            {
+                DisplayAlert("Registro de Notas", "Bienvenido :)", "Cerrar");
+                Navigation.PushAsync(new Registro(txtUsuario.Text));
+            }
+            else
+            {
+                DisplayAlert("Alerta", "Usuario No existe!!", "Cerrar");
+            }
+
+
+        }
+    }
+}
---
+++ JimmyToapanta/Registro.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace JimmyToapanta
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro( string usuario)
+        {
+            InitializeComponent();
+            lbUsuario.Text = "El usuario conectado es: " + usuario;
+
+        }
+
+        private void btnMinicial_Clicked(object sender, EventArgs e)
+        {
+            double mInicial = Convert.ToDouble(txtMinicial.Text);
+            double tCurso = 3000;
+            double cuotas = 5;
+            double vAgregado;
+            double pMensual;
+            double porcentaje =0.03;
+            double vAdicional;
+
+            vAgregado = tCurso * porcentaje;
+            vAdicional = vAgregado / cuotas;
+
+            pMensual = (mInicial / cuotas) + vAdicional;
+
+
+
+        }
+
+        private void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
